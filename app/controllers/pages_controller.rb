ed6de867abe19dab7c8b6ac3ac3ad94844575679
Jordan Ott <ottjw@yahoo.com>
2018-01-15 00:25:10 -0700

    some typos fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,14 @@ class PagesController < ApplicationController
   include PagesHelper
 
   def index
-    # unless ignore_params
-    #   if active_month?
-    #     render :index
-    #   else
-    #     @come_back = activity_months[activity_to_month[params[:page]]].first.titleize
-    #     render :inactivity
-    #   end
-    # end
+    unless ignore_params || admin_signed_in?
+      if active_month?
+        render :index
+      else
+        @come_back = activity_months[activity_to_month[params[:page]]].first.titleize
+        render :inactivity
+      end
+    end
   end
 
   private
