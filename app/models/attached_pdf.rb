cd921dd12b49cb28d089a7d96c750a73544243d7
Jordan Ott <ottjw@yahoo.com>
2018-03-24 21:21:36 -0600

    trying to deploy to heroku

--- app/models/attached_pdf.rb
+++ app/models/attached_pdf.rb
@@ -1,6 +1,7 @@
 class AttachedPdf < ApplicationRecord
   belongs_to :owner, polymorphic: true
   has_attached_file :pdf,
-                    path: "documents/pdf/:filename" 
+                    path: "documents/pdf/:filename",
+                    s3_region: "us-east-2" 
   do_not_validate_attachment_file_type :pdf
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,11 +33,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      bucket:            ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     }
-    s3_region: ENV.fetch('AWS_REGION')
   }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
