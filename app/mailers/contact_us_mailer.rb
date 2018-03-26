6dff2c81d3889c40f7f109e6919c56f01e1f5aab
Jordan Ott <ottjw@yahoo.com>
2018-03-26 01:18:30 -0600

    testing live mailing

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -2,7 +2,8 @@ class ContactUsMailer < ApplicationMailer
 
   def send_message(name:, from:, message:)
     @message = message
-    mail(to: 'michaelsonj@deseretmgt.com', subject: "Read Today Contact", from: from)
+    @name    = name
+    mail(to: 'ottjw@yahoo.com', subject: "Read Today Contact", from: from)
   end
 
 end
