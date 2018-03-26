fba94caa1ab5c07f2f935939d0e67b8866a82d07
Jordan Ott <ottjw@yahoo.com>
2018-03-26 01:36:51 -0600

    still working on mailer, for now just redirects

--- app/controllers/mail_controller.rb
+++ app/controllers/mail_controller.rb
@@ -1,7 +1,7 @@
 class MailController < ApplicationController
 
   def create
-    SendMessageService.send(params)
+    # SendMessageService.send(params)
     flash[:success] = "Message Sent!"
     redirect_to pages_index_path(page: 'contact_us')
   end
