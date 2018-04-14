03b42cd1380d80d3977ce605420c74b0b78d808d
Jordan Ott <ottjw@yahoo.com>
2018-04-14 17:50:49 -0600

    attach images to an event to be displayed

--- app/controllers/attached_images_controller.rb
+++ app/controllers/attached_images_controller.rb
@@ -1,12 +1,20 @@
 class AttachedImagesController < ApplicationController
 
   def new
-    @image    = AttachedImage.new
-    @activity = params[:activity_id]
+    @activity = Activity.find(params[:activity_id])
+    @image    = @activity.attached_images.new
   end
 
   def create
-
+    @activity = Activity.find(params[:activity_id])
+    if params[:attached_image]
+      @image       = @activity.attached_images.new
+      @image.image = params[:attached_image][:image]
+      @image.save!
+      redirect_to activity_path(@activity)
+    else
+      redirect_to activity_path(@activity)
+    end
   end
 
   def destroy
